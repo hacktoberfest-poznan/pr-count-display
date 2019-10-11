dbed793c30b03e97ea172bfb08127d7182513e90
suve <veg@svgames.pl>
2019-10-11 19:00:42 +0200

    Open a window on init

--- src/main.c
+++ src/main.c
@@ -5,8 +5,18 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 
+
+SDL_Window *Window;
+SDL_Renderer *Renderer;
+
+
 #define INIT_SDL_FLAGS (SDL_INIT_EVENTS | SDL_INIT_TIMER | SDL_INIT_VIDEO)
 #define INIT_IMG_FLAGS (IMG_INIT_JPG | IMG_INIT_PNG)
+
+#define FPS_MINIMUM 3
+#define FPS_TICKS (1000 / FPS_MINIMUM)
+
+const SDL_Colour BackgroundColour = { .r = 0x15, .g = 0x23, .b = 0x47, .a = 255 };
 
 void init_libs(void) {
 	int err;
@@ -28,9 +38,47 @@ void init_libs(void) {
 		fprintf(stderr, "TTF_Init() failed: %s\n", TTF_GetError());
 		exit(EXIT_FAILURE);
 	}
+
+	Window = SDL_CreateWindow(
+		"Pull Request Count Display",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		0, 0,
+		SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_HIDDEN
+	);
+	if(Window == NULL) {
+		fprintf(stderr, "SDL_OpenWindow() failed: %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+
+	Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
+	if(Renderer == NULL) {
+		fprintf(stderr, "SDL_CreateRenderer() failed: %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+}
+
+int quit_requested(void) {
+	SDL_Event ev;
+	while(SDL_PollEvent(&ev) > 0) {
+		if(ev.type == SDL_QUIT)
+			return 1;
+		else if(ev.type == SDL_KEYDOWN && ev.key.keysym.sym == SDLK_ESCAPE)
+			return 1;
+	}
+	
+	return 0;
+}
+
+void draw_frame(void) {
+	SDL_SetRenderDrawColor(Renderer, BackgroundColour.r, BackgroundColour.g, BackgroundColour.b, BackgroundColour.a);
+	SDL_RenderClear(Renderer);
+	SDL_RenderPresent(Renderer);
 }
 
 void deinit_libs(void) {
+	SDL_DestroyRenderer(Renderer);
+	SDL_DestroyWindow(Window);
+
 	TTF_Quit();
 	IMG_Quit();
 	SDL_Quit();
@@ -38,7 +86,15 @@ void deinit_libs(void) {
 
 int main(void) {
 	init_libs();
-	deinit_libs();
+	SDL_ShowWindow(Window);
 
+	while(!quit_requested()) {
+		draw_frame();
+		SDL_Delay(FPS_TICKS);
+	}
+
+	SDL_HideWindow(Window);
+	deinit_libs();
+	
 	return 0;
 }
